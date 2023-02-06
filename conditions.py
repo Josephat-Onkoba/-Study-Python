f5a9d1c97a7fd1bcc8b7219a3cc7af9aafcd6a96
Josephat-Onkoba <93214348+Josephat-Onkoba@users.noreply.github.com>
2023-02-06 14:41:52 +0300

    first commit

---
+++ conditions.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first Number: "))
+num2 = float(input("Enter the second Number: "))
+num3 = float(input("Enter the third Number: "))
+if (num1>num2 and num1>num3):
+    print(num1)
+elif(num2>num1 and num2>num3):
+    print(num2)
+else:
+    print(num3)
