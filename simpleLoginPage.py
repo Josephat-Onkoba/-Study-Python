f26c8b6cfe5c365b7f9747d4ad587a44df1656ba
Josephat-Onkoba <93214348+Josephat-Onkoba@users.noreply.github.com>
2023-02-07 11:32:31 +0300

    simple sign up and login program

---
+++ simpleLoginPage.py
@@ -0,0 +1,17 @@
+print("CREATE YOUR ACCOUNT BELOW ")
+Username = input("Enter Username: ")
+Password = input("Enter Password: ") 
+confirmPassword = input("Confirm your Password: ")
+
+if(Password == confirmPassword):
+    print("Acoount created successfully, login below.")
+else:
+    print("Passwords do not match")
+    exit()
+print("LOGIN HERE:")
+Username1 = input("Enter your Username:")
+Password1 = input("Enter Password: ")
+if(Username1==Username and Password1==Password):
+    print("Login successfull")
+else:
+    print("Invalid Credentials")
