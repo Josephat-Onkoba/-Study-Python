f0d2affb5499b68a443c1d2ccdb480b371e84453
Josephat-Onkoba <josephatonkoba30@gmail.com>
2023-06-28 11:15:59 +0300

    writing a file

---
+++ Python_Operating_System/Writing_files.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# Writing files using python scripts
+with open ("Python.txt", "w") as file:
+    file.write("Its elegant syntax allows developers to express concepts concisely, making it easy to write clean and maintainable code. Python's extensive standard library provides a wide range of modules and functions that enable developers to accomplish various tasks without reinventing the wheel.")
